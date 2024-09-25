c6c2edcc1c35d104f9650168b5a3ea7e2c2fe303
Frank.Markwort <frank.markwort@syzygy.de>
2024-09-25 12:27:18 +0300

    Initial test

--- src/Poseidon2/Installer.php
+++ src/Poseidon2/Installer.php
@@ -18,18 +18,18 @@ class Installer extends LibraryInstaller
 
     protected function getPackageBasePath(PackageInterface $package)
     {
-        echo $this->composer->getPackage()->getPrettyName() . " prettyName \n";
         if ($this->composer->getPackage()->getPrettyName() === 'poseidon2/poseidon') {
             $ssp_path = ".";
         } else {
             $ssp_path = $this->composer->getConfig()->get('vendor-dir') . '/poseidon2/poseidon';
         }
-
-        echo "# ssp_path:  $ssp_path \n";
-        $matches = [];
-
-
         $name = $package->getPrettyName();
+        if (preg_match('/^[a-z0-9_.\-\/]*$/', $name) == 0) {
+            throw new InvalidArgumentException(
+                sprintf('Unable to install module %s, module name must only contain characters from a-z, 0-9, "_", "." and "-".',$name)
+            );
+        }
+        $matches = [];
         if (!preg_match('@^.*/poseidon-(module|assets)-(cron|realtime)-(.+)$@', $name, $matches)) {
             throw new InvalidArgumentException(
                 sprintf(
@@ -38,24 +38,14 @@ class Installer extends LibraryInstaller
                 )
              );
         }
-        if (count($matches) !== 3) {
+        if (count($matches) !== 4) {
              throw new InvalidArgumentException();
         }
 
-        echo "# name:  $name \n";
         $moduleType = $matches[1];
         $moduleDir = $matches[2];
-         #Assert::regex(
-         #   $moduleDir,
-         #   '@^[a-z0-9_.-]*$@',
-         #   sprintf(
-         #       'Unable to install module %s, module name must only contain characters from a-z, 0-9, "_", "." and "-".',
-         #       $name
-         #   ),
-         #   InvalidArgumentException::class
-        #);
-
-        if ( ! (0 === \strpos($moduleDir, '.'))) {
+        $moduleName = $matches[3];
+        if (0 === \strpos($moduleDir, '.')) {
             throw new InvalidArgumentException(
                 sprintf(
                 'Expected a value not to start with %2$s. Got: %s',
@@ -64,14 +54,7 @@ class Installer extends LibraryInstaller
                 )
             );
         }
-        echo "count(matches): " .count($matches) ."\n";
-        echo "## matches \n";
-        echo " moduleType: $moduleType \n";
-        echo " moduleDir: $moduleDir \n";
-        echo "## matches end\n";
-
         $extraData = $package->getExtra();
-        var_dump($extraData);
         if (isset($extraData[self::MIXED_CASE])) {
             $mixedCaseModuleName = $extraData[self::MIXED_CASE];
             if (is_string($mixedCaseModuleName)) {
@@ -79,7 +62,6 @@ class Installer extends LibraryInstaller
                     sprintf('Unable to install module %s, "%s" must be a string.', $name, self::MIXED_CASE)
                 );
             }
-
             if ($moduleDir === mb_strtolower($mixedCaseModuleName, 'utf-8')) {
                 throw new InvalidArgumentException(
                     'Unable to install module %s, "%s" must match the package name except that it can contain uppercase letters.',
@@ -89,23 +71,18 @@ class Installer extends LibraryInstaller
             }
             $moduleDir = $mixedCaseModuleName;
         }
-
-          switch ($moduleType) {
+        switch ($moduleType) {
             case 'assets':
                 return $ssp_path . '/public/assets/' . $moduleDir;
             case 'module':
-                return $ssp_path . '/modules/' . $moduleDir;
+                return sprintf('%s/src/Modules/Module/%s/%s', $ssp_path, ucfirst($moduleDir), ucfirst($moduleName));
             default:
                 throw new InvalidArgumentException(sprintf('Unsupported type: %s', $moduleType));
-        }
+            }
     }
 
     public function supports($packageType):bool
     {
-        echo "# packageType: $packageType \n";
-        echo in_array($packageType, self::SUPPORTED);
-
         return in_array($packageType, self::SUPPORTED);
     }
-
 }
